199222776e5c89c82decc6b438f0acd2789668c1
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2025-12-26 18:27:42 +0100

    enable simd

--- wasm.mk
+++ wasm.mk
@@ -7,7 +7,7 @@ CFLAGS += -Wno-gnu-compound-literal-initializer -Wno-gnu-zero-variadic-macro-arg
 CFLAGS += -Ilib -fbracket-depth=1024
 CFLAGS += -D__STDC_WANT_LIB_EXT1__
 CFLAGS += -fno-builtin-pow -fno-builtin-sin -fno-builtin-cos -fno-builtin-tan -fno-builtin-fmod -fno-builtin-fmodf -fno-builtin-cosf -fno-builtin-sinf -fno-builtin-powf
-LDFLAGS = -lm -Wl,--export-table -Wl,--stack-first -Wl,-z,stack-size=8388608
+LDFLAGS = -lm -Wl,--export-table -Wl,--stack-first -Wl,-z,stack-size=8388608 -msimd128
 
 DEBUG = 1
 
@@ -38,7 +38,7 @@ run: all
 $(GAME): $(OBJ)
 	$(CC) -o $@ $^ $(CFLAGS) $(LDFLAGS)
 ifeq ($(DEBUG),0)
-	wasm-strip $@ && wasm-opt $@ -o $@ -O3 --enable-sign-ext
+	wasm-strip $@ && wasm-opt $@ -o $@ -O3 --enable-sign-ext --enable-simd
 else
 	../../tools/emscripten/tools/wasm-sourcemap.py $@ -w $@ -p $(CURDIR) -s -u ./$@.map -o $@.map --dwarfdump=../../../emsdk/upstream/bin/llvm-dwarfdump
 	# ../../tools/emscripten/tools/wasm-sourcemap.py $@ -w $@ -p $(CURDIR) -s -u ./$@.map -o $@.map --dwarfdump=/usr/bin/llvm-dwarfdump
