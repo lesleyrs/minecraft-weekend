0944a9082865713644ac3b62c339b40b1e113c0e
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2025-12-27 03:24:24 +0100

    allow compile on latest clang

--- wasm.mk
+++ wasm.mk
@@ -6,7 +6,7 @@ CFLAGS += -Wno-pointer-arith -Wno-newline-eof -Wno-unused-parameter -Wno-gnu-sta
 CFLAGS += -Wno-gnu-compound-literal-initializer -Wno-gnu-zero-variadic-macro-arguments
 CFLAGS += -Ilib -fbracket-depth=1024
 CFLAGS += -D__STDC_WANT_LIB_EXT1__
-CFLAGS += -fno-builtin-pow -fno-builtin-sin -fno-builtin-cos -fno-builtin-tan -fno-builtin-fmod -fno-builtin-fmodf -fno-builtin-cosf -fno-builtin-sinf -fno-builtin-powf
+CFLAGS += -fno-builtin-pow -fno-builtin-sin -fno-builtin-cos -fno-builtin-tan -fno-builtin-fmod -fno-builtin-fmodf -fno-builtin-cosf -fno-builtin-sinf -fno-builtin-powf -fno-builtin-tanf
 LDFLAGS = -lm -Wl,--export-table -Wl,--stack-first -Wl,-z,stack-size=8388608 -msimd128
 
 DEBUG = 1
@@ -38,7 +38,7 @@ run: all
 $(GAME): $(OBJ)
 	$(CC) -o $@ $^ $(CFLAGS) $(LDFLAGS)
 ifeq ($(DEBUG),0)
-	wasm-strip $@ && wasm-opt $@ -o $@ -O4 --enable-sign-ext --enable-simd
+	wasm-strip $@ && wasm-opt $@ -o $@ -O4 -all
 else
 	../../tools/emscripten/tools/wasm-sourcemap.py $@ -w $@ -p $(CURDIR) -s -u ./$@.map -o $@.map --dwarfdump=../../../emsdk/upstream/bin/llvm-dwarfdump
 	# ../../tools/emscripten/tools/wasm-sourcemap.py $@ -w $@ -p $(CURDIR) -s -u ./$@.map -o $@.map --dwarfdump=/usr/bin/llvm-dwarfdump
